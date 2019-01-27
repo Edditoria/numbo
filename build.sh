8aa0fde5d62b45f3f32487d0343834b5cb2b8dcd
Edditoria <edditoria@gmail.com>
2019-01-27 18:51:33 +0800

    Update coffee script to v2; Separate into 2 form: main and browser

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "Compile coffeescript files..."
 coffee -o ./lib/ -c ./src/*.coffee
-coffee -o ./dist/ -c ./src/*.coffee
-coffee -o ./docs/assets/js -c ./src/*.coffee
+coffee --transpile -o ./dist/ -c ./src/*.coffee
+coffee --transpile -o ./docs/assets/js -c ./src/*.coffee
 coffee -o ./test/ -c ./src/test/*.coffee
 echo "Compile finish."
