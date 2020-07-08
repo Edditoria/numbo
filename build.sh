8463f819be30f22340d920cc954eb2ebd0212395
Edditoria <edditoria@gmail.com>
2020-07-09 02:21:09 +0800

    Remove files not useful anymore, except <docs/>

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Compile coffeescript files..."
-coffee --transpile --output ./lib/ --compile ./src/*.coffee
-coffee --transpile --output ./docs/assets/js --compile ./src/*.coffee
-coffee --transpile --output ./test/ --compile ./src/test/*.coffee
-echo "Compile finish."
